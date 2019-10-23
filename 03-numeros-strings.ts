8a421322e2c0509c5927c09afdba723221cb0839
JennyL00 <55715205+JennyL00@users.noreply.github.com>
2019-10-23 17:50:09 -0500

    Create 03-numeros-strings.ts

---
+++ 03-numeros-strings.ts
@@ -0,0 +1,33 @@
+const numeroUno = 1;  //camelCase
+const numeroDos = 2;
+let resultado = 0;
+//SUMA
+resultado = 1 + 2;
+//RESTAR
+resultado = 1 - 2;
+//MULTIPLICAR
+resultado = 1 * 2;
+//DIVIDIR
+resultado = 1 / 2;
+//EXPONENCIAL
+resultado = 1 ^ 2;
+//MOD
+resultado = 4 % 2; //0 -> residuo
+resultado = 4 % 3; //1  -> residuo
+resultado = 7 % 3; //1
+
+//console.log ("pera" + 2);
+
+
+
+
+
+
+
+//const COLOR_ROJO = 1;  // camelCase
+//NUMEROUNO   -> todo mayuscula (separación no)
+//NUMERO_UNO -> mayusculas guion bajo
+//Numerouno   ->
+//numero_uno   -> notación de python
+//numerOunO   ->
+
