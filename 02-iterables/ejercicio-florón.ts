732d302ec4b00f6ac6f0ae95f3c810f63a902e6f
JennyL00 <55715205+JennyL00@users.noreply.github.com>
2019-11-20 17:56:12 -0500

    alcance

--- 02-iterables/ejercicio-florón.ts
+++ 02-iterables/ejercicio-florón.ts
@@ -1,6 +1,10 @@
-const arreglo = [];
 
-const indiceUno = 0;
-for(let indiceUno = 0; indiceUno <= 5; indiceUno++){
-Math.floor(Math.random() * 2)
-}
+
+
+const arreglo = [0,0,0,0,1];    // const para que no varie el tamaño del arreglo
+let indiceUno = Math.floor(Math.random() * 4) ;
+
+arreglo[indiceUno] = 1; //la posicion donde indique el indiceUno al arreglo le va a asignar un 1
+console.log(indiceUno);
+
+
