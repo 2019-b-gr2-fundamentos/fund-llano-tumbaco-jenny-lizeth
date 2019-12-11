67e4046b6ba83332e0f91714c3d3e5caab0c9391
JennyL00 <55715205+JennyL00@users.noreply.github.com>
2019-12-11 16:32:02 -0500

    listo

--- Llano-Tumbaco-Jenny-Lizeth-exámen-1/arreglo_animales.m
+++ Llano-Tumbaco-Jenny-Lizeth-exámen-1/arreglo_animales.m
@@ -1,45 +1,40 @@
 %crear los registros de un arreglo
 
 arregloAnimales = [];
+arregloAnimales=string(arregloAnimales);
 animales = input('¿Cuántos tipos de animales deseas ingresar?: ');
 
 for n=1: animales
     tiposAnimales = input('Ingresa un tipo de animal: ', 's');
     str = string(tiposAnimales);       %cambiar el string a un numero
-    arregloAnimales(n) = str;              %no acepta string
+    arregloAnimales(n) = tiposAnimales;              %no acepta string
    
     k = n;
     disp(tiposAnimales + " tiene idice igual a " + k);
 end
 disp(arregloAnimales)
+
 m =1;
+
 while m == 1
     
 des = input('¿Qué deseas realizar ahora: \n 1. Crear otro \n 2. Actualizar\n 3. Eliminar \n 4. Salir? \n >');
 
 if des == 1
-    for p=1:1
+    
     otro = input('Ingrese un tipo de animal: ', 's');
-    y = str2double(otro);
-    arregloAnimales(p) = y;
-    
-    l = animales + 1;
-    z = arregloAnimales;
-    z(l) = y;
-    
-    
+    str2 = string(otro);
+    arregloAnimales(animales + 1) = otro;
+ 
     disp(otro + " tiene idice igual a " + l);
    
-   
-    
-    end
   disp(arregloAnimales);  %revisale
 else
     if des == 2
        actualizar = input('Ingrese el índice del tipo de animal que se desea actualizar: ');
        actualizacion = input('Escribe la actualización: ','s');
-       h = str2double(actualizacion);
-       arregloAnimales(actualizar)= h;
+       str3 = string(actualizacion);
+       arregloAnimales(actualizar)= actualizacion;
        
        disp(arregloAnimales);
     else
@@ -49,6 +44,7 @@ else
            disp(arregloAnimales);
         else
             if des == 4
+                m =4;
                 disp('Gracias =)');
             else
                 disp('ERROR');
