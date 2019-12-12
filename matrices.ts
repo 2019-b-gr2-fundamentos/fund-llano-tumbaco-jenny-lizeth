4978950f29372d4820176ef448b6c9fc19c730f1
JennyL00 <55715205+JennyL00@users.noreply.github.com>
2019-12-12 16:47:43 -0500

    terminar

---
+++ matrices.ts
@@ -0,0 +1,34 @@
+//escriba una programa que verifique si dos matrices son iguales 
+//(la matriz es de dos dimensiones n*m) 
+//"n" y "m" pueden ser iguales o distintas
+
+const matrizUno = [
+    [1,2],
+    [3,4]
+]
+const matrizDos = [
+    [1,2],
+    [3,4]
+]
+
+
+function compararMatrizes(
+    matrizUno: number[][], 
+    matrizDos: number[][]
+    ): boolean {
+        
+        return true;
+
+}
+
+function main(){
+    
+}
+
+
+/*n = 2, n=3
+  1 2 3    2 3 4
+  4 5 6    4 5 6
+ * /
+ 
+//Sumar todas las filas y todas las columnas
