057024c2e9973ac9c943e39b09f8e6bd07225546
aaa <aaaaaaaaa>
2020-01-09 17:52:54 -0500

    librerias 2

--- 05-proyecto/index.js
+++ 05-proyecto/index.js
@@ -5,5 +5,7 @@ exports.__esModule = true;
 //importar todo el contenido con el nombre 'sumaLibreria'
 //del paquete 'libreria-jll/lib/suma'
 var sumaLibreria = require("libreria-jll/lib/suma"); // import para ts
+var PI = require("libreria-jll/lib/pi"); //* para importar todo
 var respuesta = sumaLibreria(1, 2);
 console.log('La respuesta es:', respuesta);
+console.log('La respuesta pi es:', PI);
--- 05-proyecto/index.ts
+++ 05-proyecto/index.ts
@@ -1,9 +1,12 @@
 //NODE MODULES
 //const sumaLibreria = require('libreria-jll/lib/suma');    --- > require para js
 
-
 //importar todo el contenido con el nombre 'sumaLibreria'
 //del paquete 'libreria-jll/lib/suma'
+
 import * as sumaLibreria from 'libreria-jll/lib/suma';  // import para ts
+import * as PI from 'libreria-jll/lib/pi';              //* para importar todo
+
 const respuesta = sumaLibreria(1,2);
 console.log('La respuesta es:',respuesta);
+console.log('La respuesta pi es:', PI);
