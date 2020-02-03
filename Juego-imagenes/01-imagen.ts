b406ddec7decd3c69c449a6a1050158e801b35e6
JennyL00 <>
2020-02-03 03:35:54 -0500

    borrador

--- Juego-imagenes/01-imagen.ts
+++ Juego-imagenes/01-imagen.ts
@@ -1,4 +1,13 @@
-export function cuadrado(){
+export function cuadrado(
+    arreglo:any,
+    funcion:(
+        valorActual:any,
+        indice:number,
+        arreglo?:any)=>boolean
+):boolean{
+    for(let i = 0;i < arreglo.length; i++){
+
+
     const lado = 11;
     let contador = 1;
         
@@ -9,8 +18,8 @@ export function cuadrado(){
     
     console.log("");
     return;
+    }
 }
-
 export function restar(numUno,numDos,valorInicial){
     return numUno - numDos;
 }
